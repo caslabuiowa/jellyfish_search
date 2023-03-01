93c645a40d9ef9add066046ce57e071ecf4ae625
magicbycalvin <magicbycalvin@gmail.com>
2023-03-01 15:07:20 -0600

    Jellyfish search now works in parallel for both number of trajectory and timeout cases.

--- jfs_core/jellyfish_search.py
+++ jfs_core/jellyfish_search.py
@@ -10,26 +10,23 @@ import bisect
 from dataclasses import dataclass
 import logging
 from multiprocessing import Pool
+import time
 
-from control import lqr
-import matplotlib.pyplot as plt
-from matplotlib.patches import Circle
-from numba import cfunc, carray
-from numbalsoda import lsoda_sig, lsoda
+# from control import lqr
+# from numba import cfunc, carray
+# from numbalsoda import lsoda_sig, lsoda
 import numpy as np
 from numpy.random import SeedSequence, default_rng
 from numpy.typing import ArrayLike
 
-from polynomial.bernstein import Bernstein
+# from polynomial.bernstein import Bernstein
 from stoch_opt.constraint_functions import CollisionAvoidance, MaximumSpeed, MaximumAngularRate, SafeSphere
 from stoch_opt.cost_functions import SumOfDistance
-from stoch_opt.utils import state2cpts
+# from stoch_opt.utils import state2cpts
 
 from apf_search import generate_apf_trajectory, generate_piecewise_apf_trajectory
 from brachistochrone_search import generate_brachistochrone_trajectory
 from lqr_search import generate_lqr_trajectory
-
-# TODO: Major refactoring required since the functions and objects are incredibly messy
 
 LOG_LEVEL = logging.WARNING
 logger = logging.getLogger(__name__)
@@ -43,125 +40,125 @@ if len(logger.handlers) < 1:
 DISABLE_CONSTRAINTS = False
 
 
-class Worker:
-    def __init__(self, trajectory_gen_fn, n_trajectories, obstacles, rsafe):
-        self.running = False
-        self.trajectories = []
+# class Worker:
+#     def __init__(self, trajectory_gen_fn, n_trajectories, obstacles, rsafe):
+#         self.running = False
+#         self.trajectories = []
 
-        self.generate_trajectory = trajectory_gen_fn
-        self.n_trajectories = n_trajectories
-        self.obstacles = obstacles
-        self.rsafe = rsafe
-        self.solver_params = {}
+#         self.generate_trajectory = trajectory_gen_fn
+#         self.n_trajectories = n_trajectories
+#         self.obstacles = obstacles
+#         self.rsafe = rsafe
+#         self.solver_params = {}
 
-    def do_work(self, safe_dist, vmax, wmax):
-        self.running = True
-        for i in range(self.n_trajectories):
-            print(f'Trajectory {i}')
-            temp_traj = self.generate_trajectory()
-            logger.debug('Checking feasibility')
-            if is_feasible(temp_traj, self.obstacles, safe_dist, vmax, wmax, self.rsafe) or DISABLE_CONSTRAINTS:
-                logger.debug('Assigning cost')
-                cost = cost_fn(temp_traj, self.solver_params['goal'])
-                bisect.insort(self.trajectories, (temp_traj, cost), key=lambda x: x[1])
+#     def do_work(self, safe_dist, vmax, wmax):
+#         self.running = True
+#         for i in range(self.n_trajectories):
+#             print(f'Trajectory {i}')
+#             temp_traj = self.generate_trajectory()
+#             logger.debug('Checking feasibility')
+#             if is_feasible(temp_traj, self.obstacles, safe_dist, vmax, wmax, self.rsafe) or DISABLE_CONSTRAINTS:
+#                 logger.debug('Assigning cost')
+#                 cost = cost_fn(temp_traj, self.solver_params['goal'])
+#                 bisect.insort(self.trajectories, (temp_traj, cost), key=lambda x: x[1])
 
-        self.running = False
-
-
-class PWWorker:
-    def __init__(self, trajectory_gen_fn, n_trajectories, obstacles, rsafe):
-        self.running = False
-        self.trajectories = []
-
-        self.generate_trajectory = trajectory_gen_fn
-        self.n_trajectories = n_trajectories
-        self.obstacles = obstacles
-        self.rsafe = rsafe
-        self.solver_params = {}
-
-    def do_work(self, safe_dist, vmax, wmax):
-        self.running = True
-        for i in range(self.n_trajectories):
-            print(f'Trajectory {i}')
-            temp_pw_traj = self.generate_trajectory()
-            logger.debug('Checking feasibility')
-
-            traj_feasibility = []
-            for traj in temp_pw_traj:
-                traj_feasibility.append(is_feasible(traj, self.obstacles, safe_dist, vmax, wmax, self.rsafe))
-
-            if np.all(traj_feasibility) or DISABLE_CONSTRAINTS:
-                logger.debug('Assigning cost')
-                cost = sum([cost_fn(traj, self.solver_params['goal']) for traj in temp_pw_traj])
-                bisect.insort(self.trajectories, (temp_pw_traj, cost), key=lambda x: x[1])
-
-        self.running = False
+#         self.running = False
 
 
-# TODO: determine whether to use t_max or n_trajectories, or figure out how to offer both
-# TODO: add a function to add/remove obstacles
-class JellyfishController:
-    def __init__(self, n, ndim, tf, obstacles, safe_dist, vmax, wmax, safe_planning_radius, t_max, n_trajectories,
-                 solver_params={}, rng_seed=None):
-        self.n = n
-        self.ndim = ndim
-        self.tf = tf
-        self.obstacles = obstacles
-        self.safe_dist = safe_dist
-        self.vmax = vmax
-        self.wmax = wmax
-        self.safe_planning_radius = safe_planning_radius
-        self.t_max = t_max
-        self.n_trajectories = n_trajectories
-        self.solver_params = solver_params
-        self.rng_seed = rng_seed
-        self.rng = default_rng(rng_seed)
+# class PWWorker:
+#     def __init__(self, trajectory_gen_fn, n_trajectories, obstacles, rsafe):
+#         self.running = False
+#         self.trajectories = []
 
-    def run_worker(self, solver_params):
-        solver_method = solver_params['method']
-        def trajectory_gen_fn():
-            if solver_method.lower() == 'lqr':
-                return generate_lqr_trajectory(solver_params['x0'],
-                                               solver_params['goal'],
-                                               solver_params['Q_std'],
-                                               solver_params['R_std'],
-                                               solver_params['x0_std'],
-                                               solver_params['tf'],
-                                               n=self.n,
-                                               rng=self.rng)
-            elif solver_method.lower() == 'brachistochrone':
-                return generate_brachistochrone_trajectory(solver_params['x0'],
-                                                           solver_params['goal'],
-                                                           solver_params['R_std'],
-                                                           n=self.n,
-                                                           rng=self.rng)
-            elif solver_method.lower() == 'apf':
-                return generate_apf_trajectory(solver_params['x0'],
-                                               solver_params['goal'],
-                                               solver_params['obstacles'],
-                                               rho_std=0.1,
-                                               tf_max=600,
-                                               n=self.n,
-                                               rng=self.rng)
-            elif solver_method.lower() == 'apf_pw':
-                return generate_piecewise_apf_trajectory(solver_params['x0'],
-                                                         solver_params['goal'],
-                                                         solver_params['obstacles'],
-                                                         rho_std=0.1,
-                                                         tf_max=600,
-                                                         n=self.n,
-                                                         rng=self.rng)
-            else:
-                raise ValueError('Incorrect solver method.')
+#         self.generate_trajectory = trajectory_gen_fn
+#         self.n_trajectories = n_trajectories
+#         self.obstacles = obstacles
+#         self.rsafe = rsafe
+#         self.solver_params = {}
 
-        if solver_method.lower() == 'apf_pw':
-            self.worker = PWWorker(trajectory_gen_fn, self.n_trajectories, self.obstacles, self.safe_planning_radius)
-        else:
-            self.worker = Worker(trajectory_gen_fn, self.n_trajectories, self.obstacles, self.safe_planning_radius)
-        self.worker.solver_params = solver_params
-        self.worker.do_work(self.safe_dist, self.vmax, self.wmax)
+#     def do_work(self, safe_dist, vmax, wmax):
+#         self.running = True
+#         for i in range(self.n_trajectories):
+#             print(f'Trajectory {i}')
+#             temp_pw_traj = self.generate_trajectory()
+#             logger.debug('Checking feasibility')
 
-        return self.worker.trajectories
+#             traj_feasibility = []
+#             for traj in temp_pw_traj:
+#                 traj_feasibility.append(is_feasible(traj, self.obstacles, safe_dist, vmax, wmax, self.rsafe))
+
+#             if np.all(traj_feasibility) or DISABLE_CONSTRAINTS:
+#                 logger.debug('Assigning cost')
+#                 cost = sum([cost_fn(traj, self.solver_params['goal']) for traj in temp_pw_traj])
+#                 bisect.insort(self.trajectories, (temp_pw_traj, cost), key=lambda x: x[1])
+
+#         self.running = False
+
+
+# # TODO: determine whether to use t_max or n_trajectories, or figure out how to offer both
+# # TODO: add a function to add/remove obstacles
+# class JellyfishController:
+#     def __init__(self, n, ndim, tf, obstacles, safe_dist, vmax, wmax, safe_planning_radius, t_max, n_trajectories,
+#                  solver_params={}, rng_seed=None):
+#         self.n = n
+#         self.ndim = ndim
+#         self.tf = tf
+#         self.obstacles = obstacles
+#         self.safe_dist = safe_dist
+#         self.vmax = vmax
+#         self.wmax = wmax
+#         self.safe_planning_radius = safe_planning_radius
+#         self.t_max = t_max
+#         self.n_trajectories = n_trajectories
+#         self.solver_params = solver_params
+#         self.rng_seed = rng_seed
+#         self.rng = default_rng(rng_seed)
+
+#     def run_worker(self, solver_params):
+#         solver_method = solver_params['method']
+#         def trajectory_gen_fn():
+#             if solver_method.lower() == 'lqr':
+#                 return generate_lqr_trajectory(solver_params['x0'],
+#                                                solver_params['goal'],
+#                                                solver_params['Q_std'],
+#                                                solver_params['R_std'],
+#                                                solver_params['x0_std'],
+#                                                solver_params['tf'],
+#                                                n=self.n,
+#                                                rng=self.rng)
+#             elif solver_method.lower() == 'brachistochrone':
+#                 return generate_brachistochrone_trajectory(solver_params['x0'],
+#                                                            solver_params['goal'],
+#                                                            solver_params['R_std'],
+#                                                            n=self.n,
+#                                                            rng=self.rng)
+#             elif solver_method.lower() == 'apf':
+#                 return generate_apf_trajectory(solver_params['x0'],
+#                                                solver_params['goal'],
+#                                                solver_params['obstacles'],
+#                                                rho_std=0.1,
+#                                                tf_max=600,
+#                                                n=self.n,
+#                                                rng=self.rng)
+#             elif solver_method.lower() == 'apf_pw':
+#                 return generate_piecewise_apf_trajectory(solver_params['x0'],
+#                                                          solver_params['goal'],
+#                                                          solver_params['obstacles'],
+#                                                          rho_std=0.1,
+#                                                          tf_max=600,
+#                                                          n=self.n,
+#                                                          rng=self.rng)
+#             else:
+#                 raise ValueError('Incorrect solver method.')
+
+#         if solver_method.lower() == 'apf_pw':
+#             self.worker = PWWorker(trajectory_gen_fn, self.n_trajectories, self.obstacles, self.safe_planning_radius)
+#         else:
+#             self.worker = Worker(trajectory_gen_fn, self.n_trajectories, self.obstacles, self.safe_planning_radius)
+#         self.worker.solver_params = solver_params
+#         self.worker.do_work(self.safe_dist, self.vmax, self.wmax)
+
+#         return self.worker.trajectories
 
 
 @dataclass
@@ -220,7 +217,54 @@ def generate_jellyfish_trajectories(problem_parameters: ProblemParameters,
     return trajectories
 
 
+def generate_jellyfish_trajectories_timeout(problem_parameters: ProblemParameters,
+                                            solver_parameters: SolverParameters,
+                                            timeout: float,
+                                            debug=False):
+    """Generate a family of trajectories using the jellyfish search approach within a given time frame
+
+    Parameters
+    ----------
+    problem_parameters : ProblemParameters
+        DESCRIPTION.
+    solver_parameters : SolverParameters
+        DESCRIPTION.
+    timeout : float
+        Generate as many trajectories as possible within the specified time.
+    debug : TYPE, optional
+        DESCRIPTION. The default is False.
+
+    Returns
+    -------
+    trajectories : TYPE
+        DESCRIPTION.
+
+    """
+    tstart = time.time()
+    trajectories = []
+    seed_seq = SeedSequence(solver_parameters.rng_seed)
+    rng_seeds = seed_seq.spawn(1000*int(timeout))
+    with Pool(solver_parameters.process_count) as pool:
+        tmp = pool.imap_unordered(function_wrapper_timeout, zip(rng_seeds,
+                                                                [problem_parameters]*len(rng_seeds),
+                                                                [solver_parameters]*len(rng_seeds),
+                                                                [debug]*len(rng_seeds)), 1)
+        for res in tmp:
+            bisect.insort(trajectories, res, key=lambda x: x[1])
+            if time.time() - tstart > timeout:
+                break
+
+    return trajectories
+
+
 def function_wrapper(rng_sequence_seed, problem_parameters, solver_parameters, debug):
+    traj = _trajectory_gen_fn(rng_sequence_seed, problem_parameters, solver_parameters, debug)
+    cost = _assess_trajectory(traj, problem_parameters)
+    return (traj, cost)
+
+
+def function_wrapper_timeout(args):
+    rng_sequence_seed, problem_parameters, solver_parameters, debug = args
     traj = _trajectory_gen_fn(rng_sequence_seed, problem_parameters, solver_parameters, debug)
     cost = _assess_trajectory(traj, problem_parameters)
     return (traj, cost)
@@ -310,7 +354,9 @@ def project_goal(x, rsafe, goal):
 
 
 if __name__ == '__main__':
-    import time
+    import matplotlib.pyplot as plt
+    from matplotlib.patches import Circle
+
     seed = 3
     Q_std = 1000
     R_std = 1000
@@ -372,12 +418,12 @@ if __name__ == '__main__':
     tik = time.time()
     traj_list = generate_jellyfish_trajectories(problem_params, solver_params)
     print(f'Elapsed time: {time.time() - tik} s')
+    print(f'{len(traj_list)=}')
 
     plt.close('all')
     fig, ax = plt.subplots()
     fig2, ax2 = plt.subplots()
-    traj_list[0][0].plot(ax, showCpts=False, color='b')
-    traj_list[0][0].diff().normSquare().plot(ax2, showCpts=False, color='b')
+
     for traj in traj_list[1:]:
         if traj[1] < np.inf:
             traj[0].plot(ax, showCpts=False, color='g', alpha=0.5)
@@ -385,12 +431,40 @@ if __name__ == '__main__':
         else:
             traj[0].plot(ax, showCpts=False, color='r', alpha=0.2)
             traj[0].diff().normSquare().plot(ax2, showCpts=False, color='r')
+
+    traj_list[0][0].plot(ax, showCpts=False, color='b', lw=2)
+    traj_list[0][0].diff().normSquare().plot(ax2, showCpts=False, color='b')
     ax.set_title('Trajectories')
     ax2.set_title('Trajectories\' Speed Squared')
 
     for obs in obstacles:
         artist = Circle(obs, radius=safe_distance)
         ax.add_artist(artist)
+
+    tik = time.time()
+    traj_list2 = generate_jellyfish_trajectories_timeout(problem_params, solver_params, 5)
+    print(f'Elapsed time: {time.time() - tik} s')
+    print(f'{len(traj_list2)=}')
+
+    fig3, ax3 = plt.subplots()
+    fig4, ax4 = plt.subplots()
+
+    for traj in traj_list2[1:]:
+        if traj[1] < np.inf:
+            traj[0].plot(ax3, showCpts=False, color='g', alpha=0.5)
+            traj[0].diff().normSquare().plot(ax4, showCpts=False, color='g')
+        else:
+            traj[0].plot(ax3, showCpts=False, color='r', alpha=0.2)
+            traj[0].diff().normSquare().plot(ax4, showCpts=False, color='r')
+
+    traj_list2[0][0].plot(ax3, showCpts=False, color='b', lw=2)
+    traj_list2[0][0].diff().normSquare().plot(ax4, showCpts=False, color='b')
+    ax.set_title('Trajectories')
+    ax2.set_title('Trajectories\' Speed Squared')
+
+    for obs in obstacles:
+        artist = Circle(obs, radius=safe_distance)
+        ax3.add_artist(artist)
 
     # solver_params = {'method': 'apf_pw',
     #                  'x0': x0,
