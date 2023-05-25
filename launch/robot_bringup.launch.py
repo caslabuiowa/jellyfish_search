55d79d4206b298ae1aba2f8d3bd680009a9dd9fa
magicbycalvin <magicbycalvin@gmail.com>
2023-05-25 12:23:26 -0500

    Finished the launch file.

---
+++ launch/robot_bringup.launch.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 11:16:22 2023
+
+@author: magicbycalvin
+"""
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+# from launch_ros.substitutions import FindPackageShare
+
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('jellyfish_search'),
+        'config',
+        'params.yaml'
+        )
+
+    jfs_controller_node = Node(
+        package='jellyfish_search',
+        namespace='',
+        executable='jellyfish_controller',
+        name='jellyfish_controller',
+        parameters=[config]
+        )
+
+    jfs_tt_node = Node(
+        package='jellyfish_search',
+        namespace='',
+        executable='trajectory_tracker',
+        name='trajectory_tracker',
+        parameters=[config]
+        )
+
+    jfs_obstacle_node = Node(
+        package='jellyfish_search',
+        namespace='',
+        executable='obstacle_detector',
+        name='obstacle_detector',
+        parameters=[config]
+        )
+
+    mavros_apm_launch = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('mavros'), 'launch', 'apm.launch')
+            ),
+        launch_arguments={'fcu_url': '/dev/ttyACM0'}
+        )
+
+    return LaunchDescription([
+        jfs_controller_node,
+        jfs_tt_node,
+        jfs_obstacle_node,
+        mavros_apm_launch
+    ])
