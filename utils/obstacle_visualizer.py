f2a2956969a3f16a62c602edd612b2838982e15b
magicbycalvin <magicbycalvin@gmail.com>
2023-05-27 11:57:20 -0500

    Added a file for visualizing the mision obstacles.

---
+++ utils/obstacle_visualizer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 27 11:31:31 2023
+
+@author: magicbycalvin
+"""
+import json
+
+from matplotlib.patches import Circle
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+if __name__ == '__main__':
+    fname = '/home/magicbycalvin/ros2_ws/src/jellyfish_search/config/obstacles.json'
+
+    with open(fname, 'r') as f:
+        obstacles = np.array(json.load(f))
+
+    plt.close('all')
+    fig, ax = plt.subplots()
+    for obs in obstacles:
+        ax.add_artist(Circle(obs[:2], radius=obs[-1]))
+    ax.set_xlim([obstacles[:, 0].min()-10, obstacles[:, 0].max()+10])
+    ax.set_ylim([obstacles[:, 1].min()-10, obstacles[:, 1].max()+10])
